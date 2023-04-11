46bf238051e3cdecf2e1f27ebedee69f164b5189
Tathagat Maitray <56931425+tathagat22@users.noreply.github.com>
2023-04-11 16:05:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Menu Dropdown Toggle
+if ($(".menu-trigger").length) {
+  $(".menu-trigger").on("click", function () {
+    $(this).toggleClass("active");
+    $(".header-area .nav").slideToggle(200);
+  });
+}
